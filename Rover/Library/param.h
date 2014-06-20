c567f1a934c815ac38914d33fcae84fc1591f1f2
Sébastien Malissard <sebastien.malissard@dbmail.com>
2014-06-20 09:36:59 +0200

    [Rover] Fix conflict in main_hardware.c and param.h

--- Rover/Library/param.h
+++ Rover/Library/param.h
@@ -1,31 +1,54 @@
+/*  Projet Rover
+Auteurs: Solana Yoann        IENAC 12L1
+         
+Date: Juin 2014
 
-
+Description: Fichier param.h
+	Macro pour un fonctionnmeent dégradé ou faire des tests
+			Activation IMU
+			Utilisation de telnet pour tester la communication
+			
+	Affichage d'information pour le debug sur:
+			la propulsion
+			la gestion d'un client
+			
+	Affichage d'information: 
+			du type message
+	        du type message recue
+	        du type message envoyé
+	        sur le thread de propulsion
+	        
+	Macro pour définir des constantes
+*/
 
 
 #ifndef PARAM_H
 #define PARMA_H
 
+
 #define I2C_OK
+//#define IMU
 //#define TELNET
-#define THREAD_PROP
 
-
+// Macro pour le DEBUG
 #define DBG_PROP
 #define DBG_CONNECTION_HANDLER
 
-#define DSPL_MSG
-
+// Macro pour l'affichage d'information
+#define THREAD_PROP // Afficher des information du tread "threadProp"
+#define DSPL_MSG          // Display all message
+#define DSPL_INFO_MAIN
 #ifdef DSPL_MSG
 	#define DSPL_RCV_MSG  // Display received message
 	#define DSPL_SND_MSG  // Display sended message
 #endif
 
+// Macro pour définir des constantes
 #define MAX_THRESHOLD_TENS  11.5
 #define MIN_THRESHOLD_TENS  10.5
 #define SEC_DSPL_TENSION       1 // Display tension of battery every minute
 #define T_INFOS             5000 // Périod 500ms for send infos
 #define DIST_MIN_SONAR        20 // In centimeter
 #define TIME_BLINK_SONAR     500 // In milisecond
-
 
 #endif // PARAM_H
